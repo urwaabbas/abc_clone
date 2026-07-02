4fd1de816140582f8e4c10ce3ba4f8983839c228
urwaabbas <urwaabbas.buzdar@gmail.com>
2026-07-02 14:22:29 +0500

    centered forms section

--- src/sections/form/forms.jsx
+++ src/sections/form/forms.jsx
@@ -1,18 +1,23 @@
+import React from "react";
+import "./forms.css";
+
 const ContactForm = () => {
   return (
     
     <div id="contact" className="contact-section">
-      <div className="contact-text">
-        <h2>Have some questions</h2>
-        <p>MESSAGE US</p>
-      </div>
-      <div className="contact-form">
-        <form>
-          <input type="text" name="name" placeholder="Name*" required />
-          <input type="email" name="email" placeholder="Email*" required />
-          <textarea name="message" placeholder="Message*" required></textarea>
-          <button type="submit">Send</button>
-        </form>
+      <div className="contact-inner">
+        <div className="contact-text">
+          <h2>Have some questions</h2>
+          <p>MESSAGE US</p>
+        </div>
+        <div className="contact-form">
+          <form>
+            <input type="text" name="name" placeholder="Name*" required />
+            <input type="email" name="email" placeholder="Email*" required />
+            <textarea name="message" placeholder="Message*" required></textarea>
+            <button type="submit">Send</button>
+          </form>
+        </div>
       </div>
     </div>
   );
--- src/sections/navbar/nav.jsx
+++ src/sections/navbar/nav.jsx
@@ -10,41 +10,45 @@ const Navbar = () => {
     <div className="the-whole-menu">
       {/* Top bar */}
       <div className="side_by_side_top_card">
-        <div className="top_nav_card">
-          <div className="sales_card">Sales Department : 3242352</div>
-          <div className="mail_card">Mail : abc@example.com</div>
-        </div>
-        <div className="otherlinks_card">
-          <img src={facebook} alt="Facebook" />
-          <img src={pinterest_logo} alt="Pinterest" />
-          <img src={xlogo} alt="X" />
+        <div className="top-bar-inner">
+          <div className="top_nav_card">
+            <div className="sales_card">Sales Department : 3242352</div>
+            <div className="mail_card">Mail : abc@example.com</div>
+          </div>
+          <div className="otherlinks_card">
+            <img src={facebook} alt="Facebook" />
+            <img src={pinterest_logo} alt="Pinterest" />
+            <img src={xlogo} alt="X" />
+          </div>
         </div>
       </div>
 
       {/* Main navigation */}
       <div className="menu-attached-with-content">
-        <img id="logo" src={logoEnside} alt="Logo" />
+        <div className="nav-inner">
+          <img id="logo" src={logoEnside} alt="Logo" />
 
-        <ul>
-          <li>
-            <a href="#about" className="navlinks">About</a>
-          </li>
-          <li>
-            <a href="#features" className="navlinks">Features</a>
-          </li>
-          <li>
-            <a href="#services" className="navlinks">Services</a>
-          </li>
-          <li>
-            <a href="#cases" className="navlinks">Cases</a>
-          </li>
-          <li>
-            <a href="#contact" className="navlinks">Contact</a>
-          </li>
-          <li>
-            <button className="navlinks">Purchase</button>
-          </li>
-        </ul>
+          <ul>
+            <li>
+              <a href="#about" className="navlinks">About</a>
+            </li>
+            <li>
+              <a href="#features" className="navlinks">Features</a>
+            </li>
+            <li>
+              <a href="#services" className="navlinks">Services</a>
+            </li>
+            <li>
+              <a href="#cases" className="navlinks">Cases</a>
+            </li>
+            <li>
+              <a href="#contact" className="navlinks">Contact</a>
+            </li>
+            <li>
+              <button className="navlinks">Purchase</button>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
   );
